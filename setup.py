10f7a3318e2aa9eea466d665a3f66eb82aede548
abdkar <am.karbalaie@gmail.com>
2026-07-06 20:13:05 +0200

    chore: update packaging configuration, remove MIT license tag, and add output directories to gitignore

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Topic :: Scientific/Engineering :: Medical Science Apps.",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
